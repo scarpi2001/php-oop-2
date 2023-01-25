6c6f6275442ff2e8366f15c0a4b9e9f0f74d4c63
Marco <posta.email3001@gmail.com>
2023-01-25 17:09:51 +0100

    definita classe Product variabili, costruttore e metodo x stampa istanziati due prodotti in array $products stampati con foreach

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+class Product
+{
+
+    public $name;
+    public $animal;
+    public $type;
+
+    public function __construct($name, $animal, $type)
+    {
+        $this->name = $name;
+        $this->animal = $animal;
+        $this->type = $type;
+    }
+
+    public function getFullProducts()
+    {
+
+        return "
+            <div>
+
+            <h1> name: " . $this->name . "</h1>"
+            . "<h2> animal: " . $this->animal . "</h2>"
+            . "<h2> type: " . $this->type . "</h2> 
+            
+            </div>
+            
+            ";
+
+    }
+
+}
+
+$products = [
+    new Product("Crocchette Carne Mix", "cani", "mangime"),
+    new Product("Crocchette Pesce Mix", "gatti", "mangime")
+];
+
+foreach ($products as $product) {
+
+    echo $product->getFullProducts();
+}
+?>
