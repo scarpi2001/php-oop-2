8b6c7dac4e03d317f415c449ffdf831666ff513f
Marco <posta.email3001@gmail.com>
2023-01-25 22:29:37 +0100

    creata struttura layout aggiunto style.scss aggiunta proprietà $price

--- db.php
+++ db.php
@@ -1,6 +1,9 @@
 <?php
 
 $products = [
-    new Product("Crocchette Carne Mix", "cani", "mangime"),
-    new Product("Crocchette Pesce Mix", "gatti", "mangime")
+    new Product("Crocchette Carne Mix", "cani", "mangimi", 8.99),
+    new Product("Crocchette Pesce Mix", "gatti", "mangimi", 8.99),
+    new Product("Pallina di gomma", "cani", "giocattoli", 3.99),
+    new Product("Cuccia", "cani", "cucce", 20),
+    new Product("Cuccia", "gatti", "cucce", 12.99)
 ];
--- models/product.php
+++ models/product.php
@@ -6,20 +6,23 @@ class Product
     public $name;
     public $animal;
     public $type;
+    public $price;
 
-    public function __construct($name, $animal, $type)
+    public function __construct($name, $animal, $type, $price)
     {
         $this->name = $name;
         $this->animal = $animal;
         $this->type = $type;
+        $this->price = $price;
     }
 
     public function getFullProducts()
     {
 
-        return "<div> <h1> name: " . $this->name . "</h1>"
-            . "<h2> animal: " . $this->animal . "</h2>"
-            . "<h2> type: " . $this->type . "</h2> </div>";
+        return "<div class='card'> <h1>" . $this->name . "</h1>"
+            . "<h2> animale: " . $this->animal . "</h2>"
+            . "<h2> tipo di articolo: " . $this->type . "</h2>"
+            . "<h4> Prezzo: €" . $this->price . "</h4> </div>";
     }
 
 }
